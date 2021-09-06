1e766c4d8e67ce96cf4d032f68b1d69f03cfd24f
DevExpressExampleBot <fakeEmail>
2021-09-06 19:59:51 +0300

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -5,25 +5,25 @@ Imports DevExpress.Xpf.Layout.Core
 Imports System.Windows.Controls
 
 Namespace DXSample
-    Partial Public Class MainWindow
-        Inherits DXWindow
+	Partial Public Class MainWindow
+		Inherits DXWindow
 
-        Private Const LayoutFileName As String = "layout.xml"
-        Public Sub New()
-            InitializeComponent()
-        End Sub
-        Private Sub OnSaveButtonClick(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
-            manager.SaveLayoutToXml(LayoutFileName)
-        End Sub
-        Private Sub OnRestoreButtonClick(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
-            manager.RestoreLayoutFromXml(LayoutFileName)
-        End Sub
-        Private Sub OnAddPanelButtonClick(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
-            Dim panel = manager.DockController.AddPanel(DockType.Fill)
-            panel.Name = "panel4"
-            panel.Caption = "Panel4"
-            manager.DockController.Dock(panel, root, DockType.Right)
-            DirectCast(sender, Button).IsEnabled = False
-        End Sub
-    End Class
+		Private Const LayoutFileName As String = "layout.xml"
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Sub OnSaveButtonClick(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+			manager.SaveLayoutToXml(LayoutFileName)
+		End Sub
+		Private Sub OnRestoreButtonClick(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+			manager.RestoreLayoutFromXml(LayoutFileName)
+		End Sub
+		Private Sub OnAddPanelButtonClick(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+			Dim panel = manager.DockController.AddPanel(DockType.Fill)
+			panel.Name = "panel4"
+			panel.Caption = "Panel4"
+			manager.DockController.Dock(panel, root, DockType.Right)
+			DirectCast(sender, Button).IsEnabled = False
+		End Sub
+	End Class
 End Namespace
