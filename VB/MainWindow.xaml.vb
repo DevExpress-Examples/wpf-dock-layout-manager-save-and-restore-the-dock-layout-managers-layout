8abf774e76f21a2c298a65204efec5ec60cb0392
DevExpressExampleBot <fakeEmail>
2022-06-29 11:44:19 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,29 +1,32 @@
-﻿Imports System
 Imports DevExpress.Xpf.Core
-Imports DevExpress.Xpf.Docking
 Imports DevExpress.Xpf.Layout.Core
 Imports System.Windows.Controls
 
 Namespace DXSample
-	Partial Public Class MainWindow
-		Inherits DXWindow
 
-		Private Const LayoutFileName As String = "layout.xml"
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Sub OnSaveButtonClick(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
-			manager.SaveLayoutToXml(LayoutFileName)
-		End Sub
-		Private Sub OnRestoreButtonClick(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
-			manager.RestoreLayoutFromXml(LayoutFileName)
-		End Sub
-		Private Sub OnAddPanelButtonClick(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
-			Dim panel = manager.DockController.AddPanel(DockType.Fill)
-			panel.Name = "panel4"
-			panel.Caption = "Panel4"
-			manager.DockController.Dock(panel, root, DockType.Right)
-			DirectCast(sender, Button).IsEnabled = False
-		End Sub
-	End Class
+    Public Partial Class MainWindow
+        Inherits ThemedWindow
+
+        Const LayoutFileName As String = "layout.xml"
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub OnSaveButtonClick(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+            Me.manager.SaveLayoutToXml(LayoutFileName)
+        End Sub
+
+        Private Sub OnRestoreButtonClick(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+            Me.manager.RestoreLayoutFromXml(LayoutFileName)
+        End Sub
+
+        Private Sub OnAddPanelButtonClick(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+            Dim panel = Me.manager.DockController.AddPanel(DockType.Fill)
+            panel.Name = "panel4"
+            panel.Caption = "Panel4"
+            Me.manager.DockController.Dock(panel, Me.root, DockType.Right)
+            CType(sender, Button).IsEnabled = False
+        End Sub
+    End Class
 End Namespace
